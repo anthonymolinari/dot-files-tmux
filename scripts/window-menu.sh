63fd33c28eee0ccf75e3fd1cb2478cfd2834b02d
anthonymolinari <molinaria@sonoma.edu>
2023-11-12 12:14:47 -0800

    updates

---
+++ scripts/window-menu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# open a popup menu with a list of active windows in this session
+tmux list-windows -F '#{ #{window_index} #{window_name} }'\
+    | awk 'BEGIN {ORS=" "} {print $2, $3 , NR, "\"select-window -t", $1 "\""}' \
+    | xargs tmux display-menu -T "Switch Window"
